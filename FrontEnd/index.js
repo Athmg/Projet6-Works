4012eba4782f19443baa635a2f946911ad8fae03
Atmg <athamidath@hotmail.com>
2023-06-28 14:34:56 +0200

    mes commits

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,203 +1,337 @@
-// recupération de l'url works
+// Récupérer l'url works
 const url = "http://localhost:5678/api/works";
 
-// recupération de la gallery
+// Récupérer le token
+const token = localStorage.getItem('token');
+
+// Récupérer éléments de la gallery
 const sectionGallery = document.querySelector(".gallery");
 
-// recupération de ID 
-const btnElt=document.getElementById("button");
+// Récupérer btn pour filter la gallery suivant les boutons
+const btnsCateg = document.querySelectorAll(".category button");
 
+// Récupérer élément bouton des categories
+const idBtnsCateg = document.getElementById('categ');
 
-/**
- * fetch get et trie de la gallery
- */
+// Récupérer éléments mode édition
+const sectionBlockHeader = document.getElementById('section_blockHeader');
+// const btnsCategory=document.getElementById('categ');
+const editIconTxt = document.querySelectorAll(".edit_icon_txt");
 
-/**/
-const main=async ()=>{
-    const response=await fetch(url);
-    const works=await response.json();
-    displayWorks(works); // affichage les données (works)
-    filterEvent(works);    
-    //token();
-   // console.log(works.token);
+// Récupérer éléments de la gallery modal
+const modalGallery = document.querySelector(".modal_gallery");
+
+// Récupérer éléments(id) du formulaire 
+/* */
+const form = document.getElementById("form");
+const imageElt = document.getElementById('image');
+const titleElt = document.getElementById('title');
+const optionsCateg = document.getElementById('id_categ_form');/**/
+
+// Récupérer éléments des modals
+const modalDelete = document.getElementById("modal-delete");
+const modalAdd = document.getElementById("modal-add");
+const closeModalBtns = document.querySelectorAll(".close");
+const btnAddPhoto = document.getElementById("btn-add-photo");
+const btnEdit = document.querySelectorAll(".btn-edit");
+
+//Récupérer éléments pour l'image et l'afficher(chargment img) dans modal ajout
+const formImglock=document.getElementById('form_img_block')
+const input = document.getElementById('image');
+const previewImgBlock = document.getElementById('preview-img-block');
+const previewImage = document.getElementById('preview-image');
+const faImage = document.getElementById('fa-i-image');
+const fileLabel = document.getElementById('img-label');
+const typeImgLabel = document.getElementById('type-img-label');
+
+// Sélectionner le bouton "Valider"
+const btnValider = document.getElementById('btn-add');
+//const formAddImage = document.querySelector('#form');
+const arrowLeft = document.getElementById('arrow-left');
+
+/*** main : fonction principale *****/
+
+const main = async () => {
+  const response = await fetch(url);
+  const works = await response.json();
+  displayWorks(works); // afficher works
+  deleteWork();        // suppression works
+  addWorks()    //ajout works
+  filterGallery(works);  // filtrer works
 };
 main();
-/**/
 
-// Parcourir tous les images  et affichage de la gallery
-function displayWorks(works){
-    sectionGallery.innerHTML ="";
-    works.forEach(work => {
-       displayWork(work);
-      // console.log(work);
-    });
-
+///////////////////////////////////////////////////////////////////
+// afficher images dans la gallery
+function displayWorks(works) {
+  sectionGallery.innerHTML = "";
+  works.forEach(work => {
+    displayWork(work);
+  });
 }
 
-//affichage d'une images dans la gallery
-function displayWork(work){
-    const modalContainer=document.querySelector('.modal_container');
-    const modalTrigger=document.querySelectorAll('.modal_trigger');
-    //modal cloner images
-     modal=document.querySelector('.modal');
-     modalGallery=document.querySelector('.modal_gallery');
-
-    // affichage sur le DOM index
-    sectionGallery.innerHTML +=`
-        <figure>
+//afficher une image dans la gallery
+function displayWork(work) {
+  // afficher sur la page index
+  sectionGallery.innerHTML += `
+            <figure>
+                <img src="${work.imageUrl}" alt="${work.title}">
+                <figcaption>${work.title}</figcaption>
+            </figure>
+        `
+  // afficher sur la modal
+  modalGallery.innerHTML += `
+          <figure>
+            <div id="id-icon"><i class="iconDelete fa-solid fa-trash-can" data-id=${work.id}></i></div>
             <img src="${work.imageUrl}" alt="${work.title}">
-            <figcaption>${work.title}</figcaption>
-        </figure>
-    `
-
-   // affichage sur la modal
-        modalTrigger.forEach(trigger => 
-            trigger.addEventListener('click',()=> {
-                toggleModal();
-                modalGallery.innerHTML +=`
-                    <figure>
-                        <span class="dialog_icon_delete">
-                        <i class="iconDelete fa-solid fa-trash-can"></i>
-                        </span> 
-                        <img src="${work.imageUrl}" alt="${work.title}">
-                        <figcaption>éditer</figcaption>
-                    </figure>
-                `
-                const iconDelete=document.querySelectorAll('.iconDelete');
-                iconDelete.addEventListener('click',()=>{
-                    console.log('suppp:' ,iconDelete);
-                })
-            }))
-        //affichage modal active
-        function toggleModal(){
-            modalContainer.classList.toggle("active");                 
-            
-        }
+            <figcaption class="caption-edit">éditer</figcaption>
+          </figure>
+        `
 }
 
-// recupération de ID 
-const btnsElt=document.querySelectorAll(".category button");
-
-function filterEvent(works){
-    btnsElt.forEach(btnElt =>{
-        btnElt.addEventListener("click", () => {
-            const categoryId = parseInt(btnElt.id);
-            filterGallery(works,categoryId);
-        });
-    })
+//////////////////////////////////////////////////////////////
+// filter la gallery 
+function  filterGallery(works) {
+  btnsCateg.forEach(btnCateg => {
+    btnCateg.addEventListener("click", () => {
+      const categoryId = parseInt(btnCateg.id);
+      filterEvent(works, categoryId);
+    });
+  })
 }
 
 //filter dans la gallery par catégories Id
-function filterGallery(works,idCategorie){
-    const galleryFilters=works.filter(work => work.categoryId === idCategorie || idCategorie===0 );
-    displayWorks(galleryFilters);
+function filterEvent(works, idCategorie) {
+  const galleryFilters = works.filter(work => work.categoryId === idCategorie || idCategorie === 0);
+  displayWorks(galleryFilters);
 
 }
 
-/**function pour recupérer le token*/
-function getToken(){
-    const tokenStorage=localStorage.getItem('token');
-    console.log("token:" , tokenStorage);
-}
-getToken();
+//////////////////////fin/////////////////////////////////
+function deleteWork() {
+  const iconDeletes = document.querySelectorAll(".iconDelete");
+  const idGallery = document.querySelectorAll('.gallery figure');
 
-/**afficher et cacher les données suivant l'etat du toke*/ 
+  iconDeletes.forEach((iconDelete, index) => {
+    iconDelete.addEventListener('click', (e) => {
+      e.preventDefault();
+      const idDelete = iconDelete.dataset.id;
+      const figure = iconDelete.parentNode.parentNode;
 
-//chargement de l'index avec le token
-//window.addEventListener("load", token());
+      if (token) {
+        fetch(`http://localhost:5678/api/works/${idDelete}`, {
+          method: 'DELETE',
+          headers: {
+            "Accept": "application/json",
+            Authorization: `Bearer ${token}`
+          }
+        })
+          .then(response => {
+            if (response.ok) {  // Suppression réussie 
+             // Supprimer la galerie dans la modal
+              figure.remove();
+              // Supprimer la galerie dans la page index
+              if (idGallery[index]) {
+                idGallery[index].remove();
+              }
+              console.log('Réponse fetch delete', response.json);
+            } else {
+              console.log('Erreur de suppression sur le serveur:', response.statusText);
+            }
+          })
+          .catch(error => {
+            console.error('Erreur de suppression:', error);
+          });
+      }
 
-function cacherBtnCategory(){
-    const tokenStorage=localStorage.getItem('token');
-    const sectionBlockHeader=document.getElementById('section_blockHeader');
-    const btnsCategory=document.getElementById('categ');
-
-    console.log(tokenStorage);   
-   // console.log(sectionBlockHeader);
-    // test du token   
-    if (tokenStorage) { 
-        console.log('login OK!!'); 
-        sectionBlockHeader.style.display ='block';
-       // donnesCacher(btnsCategory);
-        btnsCategory.style.display='none';
-       // donneesVisible(sectionBlockHeader);
-    } else { 
-        console.log('login erreur'); 
-        sectionBlockHeader.style.display ='none'
-       // donnesCacher(sectionBlockHeader);
-       // document.location.reload();
-    } 
-}
-
-/*
-* les functions pour affichage et cacher les element suivant la connexion
-*/
-
-function donneesVisible(sec){
-    //const eltVisible= document.getElementById(sec);
-   // console.log(eltVisible);
-   return (document.getElementById(sec).style.display ='block');
-}
-function donnesCacher(sec){
-  const eltInvisible= document.getElementById(sec);
-  return (eltInvisible.style.display = "none");
-   //console.log(elt.values);
+      // Empêcher la fermeture automatique de la modal
+      e.stopPropagation();
+    });
+  });
 }
 
+//////////////////////////////////////////////////////////////////////
+// Ajouter work dans le Dom et dans le server
 
+// événement pour le chagement de l'image
+input.addEventListener('change', changeImage);
 
-
-// suppression
-function deleteW(){
-    const element = document.querySelector('#delete-request-set-headers .status');
-    const tokenStorage=localStorage.getItem('token');
-const requestOptions = {
-    method: 'DELETE',
-    headers: { 
-        'Authorization': 'Bearer tokenStorage',
-        'My-Custom-Header': 'foobar'
+/* function upload l'image et l'afficher*/
+function changeImage() {
+  const file = input.files[0];
+  let imageURL = '';
+  // Vérifier si le fichier est sélectionné
+  if (file) {
+    console.log('Taille du fichier :', file.size, 'octets');
+    fileLabel.style.display = 'block';
+    // teste taille de l'image si < à 4Mo
+    if (file.size < 4) {
+      alert('verifier la taille de l image');
+      imageURL = '';
     }
-};
-    fetch('http://localhost:5678/api/works/1', )
-    .then(() => element.innerHTML = 'Delete successful');
-    console.log();
+    else {
+      // Créer l'URL pour le fichier sélectionné
+      imageURL = URL.createObjectURL(file);
+      previewImage.setAttribute('src', imageURL);
+
+      // Afficher l'image dans l'aperçu du modal
+      previewImgBlock.style.display = 'block';
+      previewImage.style.display = 'block'; 
+      formImglock.style.flexDirection='initial';
+
+      // Cacher les données présentes avant le upload
+      faImage.style.display = 'none';
+      fileLabel.style.display = 'none';
+      input.style.display = 'none';
+      typeImgLabel.style.display = 'none';
+    }
+  }
+
 }
 
+/** envoyer les données dans le serveur et ajout dans le dom**/
+function addWorks(){
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // forma data pour l'ajout
+    const formWork = new FormData(form);
+    formWork.append('title', titleElt.value);
+    console.log(titleElt);
+    formWork.append('category', optionsCateg.value);
+    console.log(optionsCateg);
+    formWork.append('image', imageElt.files[0]);
+    console.log(imageElt);
 
-
-//suprimer
-const IconDelete=document.querySelectorAll('.dialog_icon_delete');
-console.log(IconDelete);
-
-//IconDelete.addEventListener('click', deleteWorks);
-
-/**
- * Affichage de la modal
-*/
-
-//bouton acces au dialog Edit
-const btnEdit=document.querySelector('.modal_icon_edit');
-const dialog = document.querySelector('dialog'); 
-
-//btnEdit.addEventListener('click',modalShow);
-
-/**
-function modalShow() { 
-    dialog.showModal(); 
-    getWork();
-}*/
-
-
-function addIconDelete(){
-    const faRegularIcon=document.querySelector('.fa-regular');
-    console.log(faRegularIcon);
-    faRegularIcon.addEventListener('click',addIconDelete);
-    const figureElt=document.querySelector('figure');
-    console.log(figureElt);
-    const span=document.createElement('span');
-    span.innerHTML+=` 
-        <span class="dialog_icon_delete">
-        <i class="iconDelete fa-solid fa-trash-can" onclick="deleteW()"></i>
-        </span>               
-    `
-    figureElt.appendChild(span);
+    fetch('http://localhost:5678/api/works', {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${token}`,
+      },
+      body: formWork,
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((work) => {
+        // ajouter nouveau work     
+        displayWork(work)
+        form.reset(); // mettre à zéro le formulaire une fois ajouter
+        previewImage.style.display = 'none'; // Cacher l'aperçu de l'image
+        // Cacher les données présentes avant le upload
+      faImage.style.display = 'block';
+      fileLabel.style.display = 'block';
+      input.style.display = 'block';
+      typeImgLabel.style.display = 'block';
+      formImglock.style.flexDirection='column';
+      })
+      .catch((error) => {
+        console.log('Erreur lors de la requête fetch:', error);
+      });
+    e.stopPropagation();
+  });
 }
+
+//////////////////////////////////////////////////////////////////
+// changement des couleurs sur les bouttons filtrer
+function btnActive(idCateg) {
+  btnsCateg.forEach(btn => {
+    btn.addEventListener('click', function () {
+      // supprimer la classe active de tous les boutons
+      btnsCateg.forEach(btn => {
+        btn.classList.remove('active');
+      });
+      // ajouter la classe active au bouton cliqué
+      this.classList.add('active');
+
+    });
+  });
+}
+btnActive(idBtnsCateg);
+
+/////////////////////////////////////////////////////////////////
+/**afficher ou cacher : mode édition suivant l'etat du token*/
+
+// vérifier le statut de la connexion
+const testToken = loginTestToken();
+
+if (testToken) {
+  sectionBlockHeader.style.display = "block";
+  idBtnsCateg.style.display = "none";
+  console.log('connexion');
+} else {
+  sectionBlockHeader.style.display = "none";
+  idBtnsCateg.style.display = "block";
+  editIconTxt.forEach(elt => {
+    elt.style.display = "none";
+  });
+}
+
+// vérifier le Token
+function loginTestToken() {
+  // Vérifier si token existe
+  if (token) {
+    // L'utilisateur est connecté
+    return true;
+  } else {
+    // L'utilisateur est déconnecté
+    return false;
+  }
+}
+
+////////////////////////////////////////////////////////////
+// tester les champs du formulaire pour activer le bouton valider
+function testChamps() { 
+  btnValider.disabled = true;
+  if (imageElt.files[0] !== '' && titleElt.value.trim() !== '' && optionsCateg.value.trim() !== '') {
+    btnValider.disabled = false;
+  } else {
+    btnValider.disabled = true;
+  }
+}
+
+imageElt.addEventListener('change', testChamps);
+titleElt.addEventListener('change', testChamps);
+optionsCateg.addEventListener('change', testChamps);
+
+///////////////////////////////////////////////
+// événement au clic arrow-left
+arrowLeft.addEventListener("click", function () {
+  // masquer la modale ajout
+  modalAdd.style.display = "none";
+  // afficher la modale suppression
+  modalDelete.style.display = "block";
+  //formAddImage.reset();
+});
+
+///////////////////////////////////////////////
+// afficher la modal suppression
+function displayModalDelete() {
+  modalDelete.style.display = "block";
+}
+//  afficher la modal ajout
+function displayModalAdd() {
+  modalAdd.style.display = "block";
+}
+// fermer  les modals
+function fermerModals() {
+  modalDelete.style.display = "none";
+  modalAdd.style.display = "none";
+}
+// événement pour le bouton ajout photo
+btnAddPhoto.addEventListener("click", displayModalAdd);
+
+// événement pour le bouton supprimer work
+btnEdit.forEach((button) => {
+  button.addEventListener("click", displayModalDelete);
+});
+// événement pour les boutons fermer modals
+closeModalBtns.forEach((button) => {
+  button.addEventListener("click", fermerModals);
+});
+
+// événement pour fermer la modal en dehors
+window.addEventListener("click", (event) => {
+  if (event.target === modalDelete || event.target === modalAdd) {
+    fermerModals();
+  }
+});
--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,70 +1,40 @@
 const url = 'http://localhost:5678/api/users/login';
 
-
+const formElt = document.getElementById('form');
 const emailElt = document.getElementById('email');
 const passwordElt = document.getElementById('password');
-//const tokenelt=localStorage.getItem('token',JSON.parse(token));
 
-function eventSubmit() {
-    const formElt = document.getElementById('form');
-    formElt.addEventListener('submit', postForm);
-}
-eventSubmit();
-
-const optionReq = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-};
-
-const usersPost = async (body) => {
-    optionReq.body=body;
-    const response = await fetch(url, optionReq );
-    const result = await response.json();
-    console.log(result);
-    if(result.token){
-    localStorage.setItem('token',result.token);
-    window.location='modalDelete.html'
-    }
-
-    // else affichage message erreur pour l'utilisteur;
-}
-
-function testLogin(email, password) {
-    if ((email != null) && (password != null)) {
-        email = document.getElementById('email').value;
-        password = document.getElementById('password').value;
-        if ((email === 'sophie.bluel@test.tld') && (password === 'S0phie')) {
-            alert('identifiant ou le mot de passe OK!!');
-            // les indentifiants stockées dans le stroge
-            localStorage.setItem('email', email);
-            localStorage.setItem('password', password);
-            alert('les informations sont enregisrteés dans le localStorage');
-            // redirection dans la page index
-            document.location.assign('index.html');  
-        }
-        else {
-            alert('Erreur de connexion !!');
-            document.location.reload();
-        }
-    }
-     else{
-         alert('Erreur dans identifiant ou le mot de passe');
-    }
-}
-
-function postForm(e) {
+formElt.addEventListener('submit', async (e) => {
     e.preventDefault();
     const email = emailElt.value;
     const password = passwordElt.value;
-    const users = {
-        email: email,
-        password: password
+
+    testLogin(email, password);
+});
+
+async function testLogin(email, password) {
+    if (email === 'sophie.bluel@test.tld' && password === 'S0phie') {
+        localStorage.setItem('email', email);
+        localStorage.setItem('password', password);
+// appeller fetch
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, password })
+        });
+        const result = await response.json();
+        console.log(result);
+// tester si token ok
+        if (result.token) {
+            localStorage.setItem('token', result.token);
+            window.location.href = 'index.html';
+        } else {
+           // Afficher erreur dans la connexion
+            console.log('Erreur de connexion');
+           
+        }
+    } else {
+       // Afficher dans l'identification
+        console.log('Identifiant ou mot de passe invalide'); 
     }
-    const usersJson = JSON.stringify(users);
-    console.log(usersJson);
-
-   // testLogin(email, password);
-
-    //appel function pour le  fetch
-    usersPost(usersJson);
 }
