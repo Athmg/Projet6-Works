9e2062aaf92244efa13f7890d0895e8bcd99cc18
Atmg <athamidath@hotmail.com>
2023-06-29 18:00:47 +0200

    modification login et le formulaire addWorks

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -17,6 +17,8 @@ const idBtnsCateg = document.getElementById('categ');
 const sectionBlockHeader = document.getElementById('section_blockHeader');
 // const btnsCategory=document.getElementById('categ');
 const editIconTxt = document.querySelectorAll(".edit_icon_txt");
+const logout=document.getElementById('logout');
+const login=document.getElementById('login');
 
 // Récupérer éléments de la gallery modal
 const modalGallery = document.querySelector(".modal_gallery");
@@ -27,6 +29,8 @@ const form = document.getElementById("form");
 const imageElt = document.getElementById('image');
 const titleElt = document.getElementById('title');
 const optionsCateg = document.getElementById('id_categ_form');/**/
+// Sélectionner le bouton "Valider pour l'ajout"
+const btnValider = document.getElementById('btn-add');
 
 // Récupérer éléments des modals
 const modalDelete = document.getElementById("modal-delete");
@@ -34,9 +38,10 @@ const modalAdd = document.getElementById("modal-add");
 const closeModalBtns = document.querySelectorAll(".close");
 const btnAddPhoto = document.getElementById("btn-add-photo");
 const btnEdit = document.querySelectorAll(".btn-edit");
+const errorMessage = document.getElementById("error-message");
 
 //Récupérer éléments pour l'image et l'afficher(chargment img) dans modal ajout
-const formImglock=document.getElementById('form_img_block')
+const formImgBlock=document.getElementById('form_img_block')
 const input = document.getElementById('image');
 const previewImgBlock = document.getElementById('preview-img-block');
 const previewImage = document.getElementById('preview-image');
@@ -44,8 +49,7 @@ const faImage = document.getElementById('fa-i-image');
 const fileLabel = document.getElementById('img-label');
 const typeImgLabel = document.getElementById('type-img-label');
 
-// Sélectionner le bouton "Valider"
-const btnValider = document.getElementById('btn-add');
+
 //const formAddImage = document.querySelector('#form');
 const arrowLeft = document.getElementById('arrow-left');
 
@@ -157,50 +161,47 @@ function deleteWork() {
 input.addEventListener('change', changeImage);
 
 /* function upload l'image et l'afficher*/
-function changeImage() {
-  const file = input.files[0];
-  let imageURL = '';
-  // Vérifier si le fichier est sélectionné
-  if (file) {
-    console.log('Taille du fichier :', file.size, 'octets');
-    fileLabel.style.display = 'block';
-    // teste taille de l'image si < à 4Mo
-    if (file.size < 4) {
-      alert('verifier la taille de l image');
-      imageURL = '';
-    }
-    else {
-      // Créer l'URL pour le fichier sélectionné
-      imageURL = URL.createObjectURL(file);
-      previewImage.setAttribute('src', imageURL);
 
-      // Afficher l'image dans l'aperçu du modal
-      previewImgBlock.style.display = 'block';
-      previewImage.style.display = 'block'; 
-      formImglock.style.flexDirection='initial';
-
-      // Cacher les données présentes avant le upload
-      faImage.style.display = 'none';
-      fileLabel.style.display = 'none';
-      input.style.display = 'none';
-      typeImgLabel.style.display = 'none';
+  function changeImage() {
+    const file = imageElt.files[0];
+    let imageURL = '';
+    // Vérifier si le fichier est sélectionné
+    if (file) {
+      console.log('Taille du fichier :', file.size, 'octets');
+      fileLabel.style.display = 'block';
+      // Vérifier la taille de l'image si elle est inférieure à 4Mo (* 1048576)
+      if (file.size < 4 ) {
+        alert("L'image doit être inférieure à 4 Mo");
+        imageURL = '';
+      } else {
+        // Créer l'URL pour le fichier sélectionné
+        imageURL = URL.createObjectURL(file);
+        previewImage.setAttribute('src', imageURL);
+  
+        // Afficher l'image dans l'aperçu du modal
+        previewImgBlock.style.display = 'block';
+        previewImage.style.display = 'block';
+        formImgBlock.style.flexDirection = 'initial';
+  
+        // Cacher les données présentes avant le upload
+        faImage.style.display = 'none';
+        fileLabel.style.display = 'none';
+        imageElt.style.display = 'none';
+        typeImgLabel.style.display = 'none';
+      }
     }
   }
-
-}
+ 
 
 /** envoyer les données dans le serveur et ajout dans le dom**/
-function addWorks(){
+function addWorks() {
   form.addEventListener('submit', (e) => {
     e.preventDefault();
-    // forma data pour l'ajout
+    // FormData pour l'ajout
     const formWork = new FormData(form);
     formWork.append('title', titleElt.value);
-    console.log(titleElt);
-    formWork.append('category', optionsCateg.value);
-    console.log(optionsCateg);
+    formWork.append('category', category.value);
     formWork.append('image', imageElt.files[0]);
-    console.log(imageElt);
 
     fetch('http://localhost:5678/api/works', {
       method: 'POST',
@@ -213,16 +214,17 @@ function addWorks(){
         return response.json();
       })
       .then((work) => {
-        // ajouter nouveau work     
-        displayWork(work)
-        form.reset(); // mettre à zéro le formulaire une fois ajouter
+        // ajouter nouveau work
+        displayWork(work);
+        form.reset(); // mettre à zéro le formulaire une fois ajouté
         previewImage.style.display = 'none'; // Cacher l'aperçu de l'image
         // Cacher les données présentes avant le upload
-      faImage.style.display = 'block';
-      fileLabel.style.display = 'block';
-      input.style.display = 'block';
-      typeImgLabel.style.display = 'block';
-      formImglock.style.flexDirection='column';
+        faImage.style.display = 'block';
+        fileLabel.style.display = 'block';
+        imageElt.style.display = 'block';
+        typeImgLabel.style.display = 'block';
+        formImgBlock.style.flexDirection = 'column';
+        
       })
       .catch((error) => {
         console.log('Erreur lors de la requête fetch:', error);
@@ -257,10 +259,12 @@ const testToken = loginTestToken();
 if (testToken) {
   sectionBlockHeader.style.display = "block";
   idBtnsCateg.style.display = "none";
+  login.style.display="none";
   console.log('connexion');
 } else {
   sectionBlockHeader.style.display = "none";
   idBtnsCateg.style.display = "block";
+  logout.style.display="none";
   editIconTxt.forEach(elt => {
     elt.style.display = "none";
   });
@@ -280,18 +284,30 @@ function loginTestToken() {
 
 ////////////////////////////////////////////////////////////
 // tester les champs du formulaire pour activer le bouton valider
-function testChamps() { 
-  btnValider.disabled = true;
-  if (imageElt.files[0] !== '' && titleElt.value.trim() !== '' && optionsCateg.value.trim() !== '') {
+function testChamps() {
+  if (imageElt.files[0] !== '' && titleElt.value.trim() !== '' && optionsCateg.value.trim() !== ''){
     btnValider.disabled = false;
-  } else {
-    btnValider.disabled = true;
-  }
+  } 
+else {
+  btnValider.disabled = true;
 }
+}
+btnValider.disabled = true;
 
 imageElt.addEventListener('change', testChamps);
 titleElt.addEventListener('change', testChamps);
 optionsCateg.addEventListener('change', testChamps);
+
+
+/////////////////////////////////////////////////////////////////
+
+function afficherAlerte(message, type, element) {
+  element.innerHTML = ""; // Effacer le contenu précédent
+  const alerte = document.createElement("div");
+  alerte.classList.add("alert", `alert-${type}`);
+  alerte.textContent = message;
+  element.appendChild(alerte);
+}
 
 ///////////////////////////////////////////////
 // événement au clic arrow-left
--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -3,38 +3,69 @@ const url = 'http://localhost:5678/api/users/login';
 const formElt = document.getElementById('form');
 const emailElt = document.getElementById('email');
 const passwordElt = document.getElementById('password');
+const errorMessage = document.getElementById("error-message");
 
 formElt.addEventListener('submit', async (e) => {
     e.preventDefault();
     const email = emailElt.value;
     const password = passwordElt.value;
-
     testLogin(email, password);
 });
 
 async function testLogin(email, password) {
-    if (email === 'sophie.bluel@test.tld' && password === 'S0phie') {
-        localStorage.setItem('email', email);
-        localStorage.setItem('password', password);
-// appeller fetch
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ email, password })
-        });
-        const result = await response.json();
-        console.log(result);
-// tester si token ok
-        if (result.token) {
-            localStorage.setItem('token', result.token);
-            window.location.href = 'index.html';
-        } else {
-           // Afficher erreur dans la connexion
-            console.log('Erreur de connexion');
-           
+    // Réinitialiser le message d'erreur
+    errorMessage.innerHTML = "";
+    const erreur = false;
+      
+    if ((email.trim() === "") || (password.trim() === "")) {
+        afficherAlerte("Veuillez remplir tous les champs.", "danger", errorMessage);
+        erreur = true;
         }
-    } else {
-       // Afficher dans l'identification
-        console.log('Identifiant ou mot de passe invalide'); 
+    else {
+        if (email === emailElt.value && password === passwordElt.value) {
+            localStorage.setItem('email', email);
+            localStorage.setItem('password', password);   
+            // appeler fetch
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ email, password })
+            });
+            const result = await response.json();
+            console.log(result);
+    
+            // tester si le token est valide
+            if (result.token) {
+                localStorage.setItem('token', result.token);
+                window.location.href = 'index.html';
+            } else {
+                // Afficher l'erreur de connexion si le token est invalide
+                afficherAlerte('Email ou mot de passe incorrect.', "danger", errorMessage);
+            }
+        } 
     }
+}    
+
+function afficherAlerte(message, type, element) {
+    element.innerHTML = ""; // Effacer le contenu précédent
+    const alerte = document.createElement("div");
+    alerte.classList.add("alert", `alert-${type}`);
+    alerte.textContent = message;
+    element.appendChild(alerte);
 }
+
+function logout() {
+    // Réinitialiser les champs 
+    emailElt.value = '';
+    passwordElt.value = '';
+    // Evénement avant de quitter la page
+    window.addEventListener("beforeunload", () => {
+        // Réinitialiser les valeurs dans le stockage local
+        localStorage.removeItem('email');
+        localStorage.removeItem('password');
+        localStorage.clear();
+    });
+}
+
+
+
